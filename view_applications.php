680541abe4fac5a9e637271af59a2ac2858ea26f
itzmeswag <swagatatigps@gmail.com>
2026-04-20 16:04:25 +0530

    Update view_applications.php

--- view_applications.php
+++ view_applications.php
@@ -12,7 +12,7 @@ if (!isset($_SESSION['user_id']) || !isset($_SESSION['user_role']) || trim($_SES
     exit();
 }
 
-$conn = new mysqli("sql305.infinityfree.com", "if0_41614068", "Swagata1077", "if0_41614068_test");
+$conn = new mysqli("localhost", "root", "mypassword", "hirehub");
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
