357164836f92884b88a5e617c284df07076961a7
itzmeswag <swagatatigps@gmail.com>
2026-04-20 16:01:05 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("sql305.infinityfree.com", "if0_41614068", "Swagata1077", "if0_41614068_test");
+$conn = new mysqli("localhost", "root", "mypassword", "hirehub");
 
 if ($conn->connect_error) {
     die("Database connection failed: " . $conn->connect_error);
